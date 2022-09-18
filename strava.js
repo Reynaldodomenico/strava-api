875dfa54c763481ba0f34377dd83bc2b82081287
Reynaldo Domenico <reynaldodomenico@yahoo.com>
2022-09-18 14:25:54 +0200

    succesfully integrating map and route per activity

--- strava.js
+++ strava.js
@@ -5,55 +5,53 @@ function getActivites(res){
     fetch(activities_link)
         .then((res) => res.json())
         .then((data) => populateTable(data))
-        // .then(function (data){          
-        //     console.log(data.length)
-            
-        //     for(var x=0; x<data.length; x++){  
-        //         var map = L.map('map').setView(data[1].start_latlng, 11);
-
-        //         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        //         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        //         }).addTo(map);                             
-        //         console.log(data[x].map.summary_polyline)
-        //         var coordinates = L.Polyline.fromEncoded(data[x].map.summary_polyline).getLatLngs()
-        //         console.log(coordinates)
-        //         L.polyline(
-        //             coordinates,
-        //             {
-        //                 color:"green",
-        //                 weight:5,
-        //                 opacity:.7,
-        //                 lineJoin:'round'
-        //             }
-
-        //         ).addTo(map)
-        //     }
-        // })
 }
-// var table = "";   
+
 function populateTable(data){  
-   
-    for (var i = 0; i < 3; i++)
-    {           
-            var activitiesData = document.createElement('div')          
-            var totalDistance = parseInt(data[i].distance)
-            var totalMovingTime = parseInt(data[i].moving_time)
-            var date = new Date(data[i].start_date)
+    for (var i = 0; i < 5; i++){  
+            var mapVariable =['map1','map2','map']               
+            var activitiesData = document.createElement('div') 
+            var activityName =  data[i].name       
+            var totalDistance = (Math.round(parseInt(data[i].distance))/1000).toFixed(2)
+            var totalMovingTime = toHHMMSS(parseInt(data[i].moving_time))
+            var date = new Date(data[i].start_date).toLocaleString('en-US',{ 
+                day: '2-digit',
+                month: 'long',
+                year:'numeric',
+                hour12:false,
+                hour:'2-digit',
+                minute:'2-digit' 
+            })
             var elevation = data[i].total_elevation_gain
+
             var table =
             `
-            <h1>${data[i].name}</h1> 
-            <p>🏆 On ${date.toLocaleString('en-US',{ day: '2-digit',month: 'long',year:'numeric',hour12:false,hour:'2-digit',minute:'2-digit' })}
-            went ${(Math.round(totalDistance)/1000).toFixed(2)} km during ${toHHMMSS(totalMovingTime)} climbing ${elevation} meters
+            <h1>${activityName}</h1> 
+            <p>🏆 On ${date}
+            went ${totalDistance} km during ${totalMovingTime} climbing ${elevation} meters
             </p>
-            `
-            // table += "<h1>"+ data[i].name +"</h1>" 
-            // table +=  "<p>🏆 On " + date.toLocaleString('en-US',{ day: '2-digit',month: 'long',year:'numeric',hour12:false,hour:'2-digit',minute:'2-digit' }) + 
-            // " went "+ (Math.round(totalDistance)/1000).toFixed(2) + 
-            // " km during " + toHHMMSS(totalMovingTime)+  " climbing " + elevation +
-            // " meters</p>"                                       
-            activitiesData.innerHTML = table;
-            document.getElementById("activities").append(activitiesData)
+            <div id="${mapVariable[i]}"><div>
+            `                                            
+            activitiesData.innerHTML = table;               
+            document.getElementById("activities").append(activitiesData)      
+
+            mapVariable[i] = L.map(`${mapVariable[i]}`).setView(data[i].start_latlng, 12)                
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(mapVariable[i]); 
+
+            var coordinates = L.Polyline.fromEncoded(data[i].map.summary_polyline).getLatLngs()
+            console.log(coordinates)
+            L.polyline(
+                    coordinates,
+                    {
+                        color:"green",
+                        weight:5,
+                        opacity:.7,
+                        lineJoin:'round'
+                    }
+
+            ).addTo(mapVariable[i])                   
             console.log(activitiesData)
     }
 }
